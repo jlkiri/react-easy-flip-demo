e42861b87afbf5608da389975eedc69dd21a39cb
Kirill Vasiltsov <v_kirill@yumemi.co.jp>
2020-05-11 18:57:06 +0900

    style tinkering

--- pages/height.tsx
+++ pages/height.tsx
@@ -1,4 +1,4 @@
-import { FlipProvider, useFlip } from "react-easy-flip";
+import { FlipProvider, useFlip, easeOutCubicCSS } from "react-easy-flip";
 import { useState } from "react";
 
 export default function Height() {
@@ -7,19 +7,33 @@ export default function Height() {
 
   useFlip(flipRootId, { duration: 1000 }, 0);
 
-  const className = `bg-gray-400 ${expanded ? "h-64 w-64" : "h-56 w-56"} flex`;
+  const className = `transition-transform ease-linear duration-500 bg-gray-400 ${
+    expanded && "expanded"
+  } flex`;
+
+  const inversionConfig = {
+    duration: 500,
+    scale: 3,
+    easing: easeOutCubicCSS,
+  };
 
   return (
     <FlipProvider>
-      <div data-flip-root-id={flipRootId}>
+      <div className="h-full w-full" data-flip-root-id={flipRootId}>
         <div
           style={{ transformOrigin: "top left" }}
           className={className}
-          data-flip-id={"a"}
+          onClick={() => setExpanded(!expanded)}
         >
-          <span>Lol</span>
+          <span
+            data-counter-scale
+            className={`transition-transform ease-linear duration-500 ${
+              expanded && "shrinked"
+            }`}
+          >
+            Lol
+          </span>
         </div>
-        <button onClick={() => setExpanded(!expanded)}>expand</button>
       </div>
     </FlipProvider>
   );
